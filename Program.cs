b74068bc7471c4a69101e2858f5344a82ece80b6
Çağdaş ÖZER <cagdas.1997@hotmail.com>
2022-04-21 17:39:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "C#";
+            string kurs2 = "Algoritma";
+            string kurs3 = "Java";
+
+            //array - dizi 
+
+            string[] kurslar = new string[] {  "Algoritma", "Java", "Python", "C++" };
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("------$$------");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Sayfa Sonu - footer");
+        }
+    }
+}
